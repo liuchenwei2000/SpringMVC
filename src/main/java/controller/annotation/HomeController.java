8bc95224f3bf97740304fa88c63143155cd08058
Vic Liu <liuchenwei2000@163.com>
2017-04-27 14:13:25 +0800

    refactoring complete

--- src/main/java/controller/annotation/HomeController.java
+++ src/main/java/controller/annotation/HomeController.java
@@ -1,5 +1,7 @@
 package controller.annotation;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -14,12 +16,14 @@ import util.SpringUtil;
  */
 @Controller 
 public class HomeController {
+
+    private static Logger LOGGER = LoggerFactory.getLogger(HomeController.class);
 	
 	@RequestMapping(value = { "/", "/home", "/index" })
 	public ModelAndView index() {
         // 通过 SpringUtil 可以获取到 Spring 容器管理的 Bean
-        System.out.println(SpringUtil.getBean("foo"));
-        System.out.println(SpringUtil.getBean("bar"));
+        LOGGER.info(SpringUtil.getBean("foo").toString());
+        LOGGER.info(SpringUtil.getBean("bar").toString());
         return new ModelAndView("index");
 	}
 	
